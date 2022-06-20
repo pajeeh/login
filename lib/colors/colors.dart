646780e7840b9953da0d96ad2fbc167826fc339f
pajeeh <l256647@dac.unicamp.br>
2022-06-20 10:13:44 -0300

    new

---
+++ lib/colors/colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+// Cores utilizadas no aplicativo
+
+const verde = Color.fromARGB(255, 0, 191, 51);
+const verdeMuitoEscuro = Color.fromARGB(255, 0, 61, 15);
+const verdeNeon = Color.fromARGB(255, 0, 255, 26);
+const verdeClaro = Color.fromARGB(255, 100, 255, 97);
+const verdeEscuro = Color.fromARGB(255, 16, 91, 17);
+const verdeHacker = Color.fromARGB(255, 0, 255, 47);
+const verdeHackerNeon = Color.fromARGB(255, 0, 255, 42);
+
+const vermelho = Color(0xFFD50000);
+const vermelhoEscuro = Color(0xFFB71C1C);
+const vermelhoNeon = Color(0xFFE53935);
+
+const azul = Color(0xFF1A237E);
+const azulEscuro = Color(0xFF0D47A1);
+const azulNeon = Color.fromARGB(255, 1, 166, 255);
+
+const preto = Color(0xFF000000);
+const branco = Color(0xFFFFFFFF);
+const transparente = Color(0x00000000);
+const cinzaBox = Color.fromARGB(255, 162, 162, 162);
+const cinza = Color.fromARGB(255, 200, 200, 200);
+const cinzaEscuro = Color.fromARGB(255, 100, 100, 100);
